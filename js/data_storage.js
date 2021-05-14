e56e7b2b513d57616b235757469022a7f13a5474
Buzz <davidbuzz@gmail.com>
2021-05-15 09:03:28 +1000

    accept older firmware version/s since 3.x isn't released for matekf7wing yet

--- js/data_storage.js
+++ js/data_storage.js
@@ -2,7 +2,7 @@
 
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
-    'minfirmwareVersionAccepted': '3.0.0',
+    'minfirmwareVersionAccepted': '2.6.0',
     'maxFirmwareVersionAccepted': '3.1.0', // Condition is < (lt) so we accept all in 3.0 branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
