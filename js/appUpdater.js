e195583adb12a94f8c858ee5ddb97b8f2a3f5164
Buzz <davidbuzz@gmail.com>
2021-06-29 20:44:05 +1000

    adjust updater to do beta/pre-release mostly the same as releases.
    and display the github release info in the dialog box.

--- js/appUpdater.js
+++ js/appUpdater.js
@@ -19,12 +19,40 @@ appUpdater.checkRelease = function (currVersion) {
 
         newVersion = releaseData[0].tag_name;
         newPrerelase = releaseData[0].prerelease;
+        let releaseHeader = releaseData[0].name;
+        let releaseBody = releaseData[0].body;
 
+        //GUI.log('current released version is:'+newVersion+" "+newPrerelase);
+
+        // beta/pre-release
+        if (newPrerelase == true && semver.gt(newVersion, currVersion)) {
+            //GUI.log(newVersion, chrome.runtime.getManifest().version);
+            //GUI.log(currVersion);
+
+            $('h1.modal__title').html("New BETA Configurator version available -->  "+newVersion);
+            $('div.modal__text').html("Release Notes:<br>"+releaseHeader+"<br>"+releaseBody);
+
+            GUI.log('New BETA version available! -->'+newVersion);
+            modalStart = new jBox('Modal', {
+                width: 600,
+                height: 400,
+                animation: false,
+                closeOnClick: false,
+                closeOnEsc: true,
+                content: $('#appUpdateNotification')
+            }).open();  // buzz todo we probably shouldn't bug users with beta's as much as releases?
+        }
+
+        // release version
         if (newPrerelase == false && semver.gt(newVersion, currVersion)) {
-            GUI.log(newVersion, chrome.runtime.getManifest().version);
-            GUI.log(currVersion);
+            //GUI.log(newVersion, chrome.runtime.getManifest().version);
+            //GUI.log(currVersion);
 
-            GUI.log('New version available!');
+            $('h1.modal__title').html("New RELEASE Configurator version available -->  "+newVersion);
+            $('div.modal__text').html("Release Notes:<br>"+releaseHeader+"<br>"+releaseBody);
+
+
+            GUI.log('New RELEASE version available! -->'+newVersion);
             modalStart = new jBox('Modal', {
                 width: 400,
                 height: 200,
