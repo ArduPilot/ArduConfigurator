59a52253232011acf009265eb3eef60534301601
Buzz <davidbuzz@gmail.com>
2021-07-06 08:47:31 +1000

    add arducopter FRAME_CLASS and FRAME_TYPE definitions, and remove frame definitions we cant do.

--- js/model.js
+++ js/model.js
@@ -40,7 +40,49 @@ const
     PLATFORM_BOAT           = 5,
     PLATFORM_OTHER          = 6;
 
-// generate mixer
+// cut-n-pasted from https://ardupilot.org/copter/docs/parameters.html#frame-class july 2021
+// values for FRAME_CLASS param.
+const ardu_frame_classes = {
+    0:	'Undefined',
+    1:	'Quad',
+    2:	'Hexa',
+    3:	'Octa',
+    4:	'OctaQuad',
+    5:	'Y6',
+    6:	'Heli',
+    7:	'Tri',
+    8:	'SingleCopter',
+    9:	'CoaxCopter',
+    10:	'BiCopter',
+    11:	'Heli_Dual',
+    12:	'DodecaHexa',
+    13:	'HeliQuad',
+    14:	'Deca',
+    15:	'Scripting Matrix',
+    16:	'6DoF Scripting'
+};
+// cut-n-pasted from https://ardupilot.org/copter/docs/parameters.html#frame-type july 2021
+// values for FRAME_TYPE param.
+const ardu_frame_types = {
+    0:	'Plus',
+    1:	'X',
+    2:	'V',
+    3:	'H',
+    4:	'V-Tail',
+    5:	'A-Tail',
+    10:	'Y6B',
+    11:	'Y6F',
+    12:	'BetaFlightX',
+    13:	'DJIX',
+    14:	'ClockwiseX',
+    15:	'I',
+    18:	'BetaFlightXReversed',
+    99: '' //use this for 'undefined' or 'unused'
+    
+};
+
+
+// generate mixer, a mixER basically defines how many MOTORS and SERVOs you have and the 'range' of min/max/reverse/etc for each of these.
 const mixerList = [
     {
         id: 1,
@@ -50,6 +92,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_TRICOPTER,
+        frame_class: 'Tri', //FRAME_CLASS=7
+        frame_type: '',  //FRAME_TYPE=???  buzz todo is -1 ok here?
         motorMixer: [
             new MotorMixRule(1.0, 0.0, 1.333333, 0.0),     // REAR
             new MotorMixRule(1.0, -1.0, -0.666667, 0.0),   // RIGHT
@@ -67,6 +111,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Quad', //FRAME_CLASS=1
+        frame_type: 'X',  //FRAME_TYPE=1
         motorMixer: [
             new MotorMixRule(1.0, -1.0, 1.0, -1.0),          // REAR_R
             new MotorMixRule(1.0, -1.0, -1.0, 1.0),          // FRONT_R
@@ -83,6 +129,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Quad', //FRAME_CLASS=1
+        frame_type: 'Plus',  //FRAME_TYPE=0
         motorMixer: [
             new MotorMixRule(1.0, 0.0, 1.0, -1.0),          // REAR
             new MotorMixRule(1.0, -1.0, 0.0, 1.0),          // RIGHT
@@ -91,28 +139,28 @@ const mixerList = [
         ],
         servoMixer: []
     },               // 2
-    {
-        id: 4,
-        name: 'Bicopter',
-        model: 'custom',
-        image: 'bicopter',
-        enabled: false,
-        legacy: true,
-        platform: PLATFORM_MULTIROTOR,
-        motorMixer: [],
-        servoMixer: []
-    },           // 4
-    {
-        id: 5,
-        name: 'Gimbal',
-        model: 'custom',
-        image: 'custom',
-        enabled: false,
-        legacy: true,
-        platform: PLATFORM_OTHER,
-        motorMixer: [],
-        servoMixer: []
-    },               // 5
+    // {
+    //     id: 4,
+    //     name: 'Bicopter',
+    //     model: 'custom',
+    //     image: 'bicopter',
+    //     enabled: false,
+    //     legacy: true,
+    //     platform: PLATFORM_MULTIROTOR,
+    //     motorMixer: [],
+    //     servoMixer: []
+    // },           // 4
+    // {
+    //     id: 5,
+    //     name: 'Gimbal',
+    //     model: 'custom',
+    //     image: 'custom',
+    //     enabled: false,
+    //     legacy: true,
+    //     platform: PLATFORM_OTHER,
+    //     motorMixer: [],
+    //     servoMixer: []
+    // },               // 5
     {
         id: 6,
         name: 'Y6',
@@ -121,6 +169,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Y6', //FRAME_CLASS=5
+        frame_type: 'X',  //X=1=Y6A. FRAME_TYPE=1  ( alternative could be 'Y6B'=10 FRAME_TYPE=10 )
         motorMixer: [
             new MotorMixRule(1.0, 0.0, 1.333333, 1.0),      // REAR
             new MotorMixRule(1.0, -1.0, -0.666667, -1.0),   // RIGHT
@@ -139,6 +189,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Hexa', //FRAME_CLASS=2
+        frame_type: 'Plus',  //FRAME_TYPE=0
         motorMixer: [
             new MotorMixRule(1.0, -0.866025, 0.5, 1.0),     // REAR_R
             new MotorMixRule(1.0, -0.866025, -0.5, -1.0),   // FRONT_R
@@ -187,22 +239,24 @@ const mixerList = [
             new ServoMixRule(SERVO_FLAPPERON_2, INPUT_STABILIZED_PITCH, 50, 0),
         ]
     },     // 27
-    {
-        id: 9,
-        name: 'Y4',
-        model: 'y4',
-        image: 'y4',
-        enabled: true,
-        legacy: true,
-        platform: PLATFORM_MULTIROTOR,
-        motorMixer: [
-            new MotorMixRule(1.0, 0.0, 1.0, -1.0),          // REAR_TOP CW
-            new MotorMixRule(1.0, -1.0, -1.0, 0.0),          // FRONT_R CCW
-            new MotorMixRule(1.0, 0.0, 1.0, 1.0),          // REAR_BOTTOM CCW
-            new MotorMixRule(1.0, 1.0, -1.0, 0.0),          // FRONT_L CW
-        ],
-        servoMixer: []
-    },                           // 9
+    // {
+    //     id: 9,
+    //     name: 'Y4',
+    //     model: 'y4',
+    //     image: 'y4',
+    //     enabled: true,
+    //     legacy: true,
+    //     platform: PLATFORM_MULTIROTOR,
+    //     frame_class: '', //FRAME_CLASS=???
+    //     frame_type: '',  //FRAME_TYPE=???
+    //     motorMixer: [
+    //         new MotorMixRule(1.0, 0.0, 1.0, -1.0),          // REAR_TOP CW
+    //         new MotorMixRule(1.0, -1.0, -1.0, 0.0),          // FRONT_R CCW
+    //         new MotorMixRule(1.0, 0.0, 1.0, 1.0),          // REAR_BOTTOM CCW
+    //         new MotorMixRule(1.0, 1.0, -1.0, 0.0),          // FRONT_L CW
+    //     ],
+    //     servoMixer: []
+    // },                           // 9
     {
         id: 10,
         name: 'Hex X',
@@ -211,6 +265,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Hexa', //FRAME_CLASS=2
+        frame_type: 'X',  //FRAME_TYPE=1
         motorMixer: [
             new MotorMixRule(1.0, -0.5, 0.866025, 1.0),     // REAR_R
             new MotorMixRule(1.0, -0.5, -0.866025, 1.0),     // FRONT_R
@@ -223,12 +279,14 @@ const mixerList = [
     },                  // 10
     {
         id: 11,
-        name: 'Octo X8',
+        name: 'Octo X8', // 8 arms, not 4
         model: 'custom',
         image: 'octo_x8',
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Octa', //FRAME_CLASS=3
+        frame_type: 'X',  //FRAME_TYPE=1
         motorMixer: [
             new MotorMixRule(1.0, -1.0, 1.0, -1.0),          // REAR_R
             new MotorMixRule(1.0, -1.0, -1.0, 1.0),          // FRONT_R
@@ -243,12 +301,14 @@ const mixerList = [
     },             // 11
     {
         id: 12,
-        name: 'Octo Flat +',
+        name: 'Octo Flat +',  // 8 arms, not 4
         model: 'custom',
         image: 'octo_flat_p',
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'Octa', //FRAME_CLASS=3
+        frame_type: 'Plus',  //FRAME_TYPE=0
         motorMixer: [
             new MotorMixRule(1.0, 0.707107, -0.707107, 1.0),    // FRONT_L
             new MotorMixRule(1.0, -0.707107, -0.707107, 1.0),    // FRONT_R
@@ -263,13 +323,16 @@ const mixerList = [
     },     // 12
     {
         id: 13,
-        name: 'Octo Flat X',
+        name: 'Octo-Quad X', // 8 motors,4 arms
         model: 'custom',
-        image: 'octo_flat_x',
+        image: 'tba',  // was octo_flat_x
         enabled: true,
         legacy: true,
         platform: PLATFORM_MULTIROTOR,
+        frame_class: 'OctaQuad', //FRAME_CLASS=4
+        frame_type: 'X',  //FRAME_TYPE=1
         motorMixer: [
+            // buzz todo this entire set of 8 mixers is probably wrong it was for a octo_flat_x
             new MotorMixRule(1.0, 1.0, -0.414178, 1.0),      // MIDFRONT_L
             new MotorMixRule(1.0, -0.414178, -1.0, 1.0),      // FRONT_R
             new MotorMixRule(1.0, -1.0, 0.414178, 1.0),      // MIDREAR_R
@@ -311,6 +374,8 @@ const mixerList = [
         enabled: false,
         legacy: true,
         platform: PLATFORM_HELICOPTER,
+        frame_class: 'Heli', //FRAME_CLASS=6
+        frame_type: '',  //FRAME_TYPE=???
         motorMixer: [],
         servoMixer: []
     },             // 15
@@ -322,6 +387,8 @@ const mixerList = [
         enabled: false,
         legacy: true,
         platform: PLATFORM_HELICOPTER,
+        frame_class: 'Heli', //FRAME_CLASS=6
+        frame_type: '',  //FRAME_TYPE=???
         motorMixer: [],
         servoMixer: []
     },              // 16
@@ -359,39 +426,39 @@ const mixerList = [
     //     ],
     //     servoMixer: []
     // },                // 18
-    {
-        id: 17,
-        name: 'PPM to SERVO',
-        model: 'custom',
-        image: 'custom',
-        enabled: false,
-        legacy: true,
-        platform: PLATFORM_OTHER,
-        motorMixer: [],
-        servoMixer: []
-    },         // 17
-    {
-        id: 18,
-        name: 'Dualcopter',
-        model: 'custom',
-        image: 'custom',
-        enabled: false,
-        legacy: true,
-        platform: PLATFORM_MULTIROTOR,
-        motorMixer: [],
-        servoMixer: []
-    },           // 18
-    {
-        id: 19,
-        name: 'Singlecopter',
-        model: 'custom',
-        image: 'custom',
-        enabled: false,
-        legacy: true,
-        platform: PLATFORM_MULTIROTOR,
-        motorMixer: [],
-        servoMixer: []
-    },         // 19
+    // {
+    //     id: 17,
+    //     name: 'PPM to SERVO',
+    //     model: 'custom',
+    //     image: 'custom',
+    //     enabled: false,
+    //     legacy: true,
+    //     platform: PLATFORM_OTHER,
+    //     motorMixer: [],
+    //     servoMixer: []
+    // },         // 17
+    // {
+    //     id: 18,
+    //     name: 'Dualcopter',
+    //     model: 'custom',
+    //     image: 'custom',
+    //     enabled: false,
+    //     legacy: true,
+    //     platform: PLATFORM_MULTIROTOR,
+    //     motorMixer: [],
+    //     servoMixer: []
+    // },           // 18
+    // {
+    //     id: 19,
+    //     name: 'Singlecopter',
+    //     model: 'custom',
+    //     image: 'custom',
+    //     enabled: false,
+    //     legacy: true,
+    //     platform: PLATFORM_MULTIROTOR,
+    //     motorMixer: [],
+    //     servoMixer: []
+    // },         // 19
     {
         id: 20,
         name: 'QuadPlane - no tilting props, 1 forward + 4 lifting',
@@ -400,6 +467,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_AIRPLANE, //PLATFORM_MULTIROTOR,
+        // frame_class: '', //FRAME_CLASS=???
+        // frame_type: '',  //FRAME_TYPE=???
         motorMixer: [
             new MotorMixRule(1.0, 0.0, 0.0, 0.0),          // front buzz todo
             new MotorMixRule(1.0, 0.0, 1.0, 1.0),          // REAR_R
@@ -422,6 +491,8 @@ const mixerList = [
         enabled: true,
         legacy: true,
         platform: PLATFORM_AIRPLANE, //PLATFORM_MULTIROTOR,
+        // frame_class: '', //FRAME_CLASS=???
+        // frame_type: '',  //FRAME_TYPE=???
         motorMixer: [
             new MotorMixRule(1.0, 0.0, 1.0, 1.0),          // REAR_R // buzz todo tilt
             new MotorMixRule(1.0, -1.0, -1.0, 0.0),        // FRONT_R
@@ -443,6 +514,8 @@ const mixerList = [
     //     enabled: true,
     //     legacy: true,
     //     platform: PLATFORM_AIRPLANE, //PLATFORM_MULTIROTOR,
+        // frame_class: '', //FRAME_CLASS=???
+        // frame_type: '',  //FRAME_TYPE=???
     //     motorMixer: [
     //         new MotorMixRule(1.0, 0.0, 0.0, 0.0),          // front buzz todo
     //         new MotorMixRule(1.0, 0.0, 1.0, 1.0),          // REAR_R
@@ -509,6 +582,8 @@ const mixerList = [
         enabled: false,
         legacy: true,
         platform: PLATFORM_TRICOPTER,
+        frame_class: 'Tri', //FRAME_CLASS=7
+        frame_type: '',  //FRAME_TYPE=???
         motorMixer: [],
         servoMixer: []
     },      // 26
@@ -659,7 +734,9 @@ const platformList = [
         id: 0,
         name: "Multirotor",
         enabled: true,
-        flapsPossible: false
+        flapsPossible: false,
+        // frame_class_possible: true,
+        // frame_type_possible: true
     },
     {
         id: 1,
@@ -671,13 +748,17 @@ const platformList = [
         id: 2,
         name: "Helicopter",
         enabled: false,
-        flapsPossible: false
+        flapsPossible: false,
+        // frame_class_possible: true,
+        // frame_type_possible: true
     },
     {
         id: 3,
         name: "Tricopter",
         enabled: true,
-        flapsPossible: false
+        flapsPossible: false,
+        // frame_class_possible: true,
+        // frame_type_possible: true
     },
     {
         id: 4,
